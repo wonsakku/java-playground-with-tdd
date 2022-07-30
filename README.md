3c7ad44fd2652fb5543208f0168e347c148b5acf
sakku <kws1044@gmail.com>
2022-07-30 14:13:08 +0900

    project init

---
+++ README.md
@@ -0,0 +1,1 @@
+# 이 코드는 [nextstep - 자바 플레이그라운드 with TDD, 클린코드](https://edu.nextstep.camp/c/9WPRB0ys/) 강의 내용입니다.
---
+++ java-baseball-playground/src/test/java/_EmptyClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _EmptyClassTest {
+
+    @Test
+    void test(){
+        assertThat(1).isEqualTo(1);
+    }
+}
